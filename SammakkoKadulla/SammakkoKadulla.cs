ea7ebea87e88939c5d7141e03fb997985cc03635
Ali Nadhum <i0233@student.jamk.fi>
2016-10-19 08:36:28 +0300

    Rotation added to the game

--- SammakkoKadulla/SammakkoKadulla.cs
+++ SammakkoKadulla/SammakkoKadulla.cs
@@ -15,14 +15,18 @@ public class SammakkoKadulla : PhysicsGame
 	private Image taustaKuva; //Taustakuvaa varten 
 	private Image[] pyorailjat;
 	private Image[] liikenneAutot;
+	private Image[] sammakkoAnimKuvat;
+	private Image[] poliisiAutoAnim;
+	private Image pensasVaaleaKuva;
+	private Image pensasTummaKuva;
 	private double LiikkumisNopeus = 800;
 	private double loikkimisNopeus = 400; 
 	private PlatformCharacter sammakko; //Sammakon hahmo
 	private Vector paikka = new Vector (0, -350); 
-	private Image[] sammakkoAnimKuvat;
-	private Image[] poliisiAutoAnim;
 	private bool flipped = false;
+	private Angle kulma;
 	#endregion
+
 
 	#region Pelin alustaminen
 	public override void Begin ()
@@ -31,14 +35,17 @@ public class SammakkoKadulla : PhysicsGame
 		PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
 		Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
 		LataaKuvat();
-		LuoTaustaKuva();
+		LuoTaustaKuva(1000, 800);
+		LuoPensaat (100,100,50,50);
 		LuoSammakko(paikka,90,90);
 		Camera.ZoomFactor = 1.2;
 		Camera.StayInLevel = true;
 		Camera.Follow(sammakko);
+		kulma = new Angle ();
 		MessageDisplay.Add( "Nappaa kärppästä!!" );
 	}
 	#endregion
+
 
 	#region Kuvan haku
 	public void LataaKuvat()
@@ -66,26 +73,31 @@ public class SammakkoKadulla : PhysicsGame
 			"SinAuto1",
 			"Poliisi1.1"
 		);
+		pensasVaaleaKuva = LoadImage ("Pensaat/Pensas1");
+		pensasTummaKuva = LoadImage ("Pensaat/Pensas2");
 		 
 	}
 	#endregion
+
 
 	public void LuoSammakko(Vector paikka, double leveys, double korkeus)
 	{
 		sammakko = new PlatformCharacter(leveys, korkeus);
 		sammakko.Image = LoadImage ("Sammakko/sammakko_0");
 		sammakko.Position = paikka;
+		sammakko.Mass = 0.1;
 		Add(sammakko);
 		LiikutaSammakko ();
 	}
 
 
-	public void LuoTaustaKuva (){
-	 Level.Background.Image = taustaKuva;
-	 Level.Background.Height = 800;
-	 Level.Background.Width = 1000;
-	 Level.CreateBorders ();
+	public void LuoTaustaKuva (int leveys, int korkeus){
+	 	Level.Background.Image = taustaKuva;
+		Level.Background.Width = leveys;
+		Level.Background.Height = korkeus;
+	 	Level.CreateBorders ();
 	}
+
 
 	public void LiikutaSammakko (){
 		Keyboard.Listen (Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
@@ -102,23 +114,40 @@ public class SammakkoKadulla : PhysicsGame
 
 	void LiikutaVasemmalle( PlatformCharacter hahmo, double nopeus)
 	{
+		if (kulma.Degrees == 0) 
+		{
+			kulma.Degrees = 90;
+		} 
+		else if (kulma.Degrees > 0 && kulma.Degrees < 90)
+		{
+			kulma.Degrees = -90;
+		}
+		hahmo.Angle = kulma;
 		hahmo.Walk (nopeus);
-		MessageDisplay.Add (hahmo.Angle.Degrees.ToString());
 		AnimoiSammakko (sammakko.Animation = new Animation (sammakkoAnimKuvat), true);
 	}
+
 
 	void LiikutaOikealle( PlatformCharacter hahmo, double nopeus)
 	{
-		Direction oikea = new Direction ();
-		KaannaSammakko (oikea);
-		MessageDisplay.Add (hahmo.Angle.Degrees.ToString());
+		if (kulma.Degrees == 0) 
+		{
+			kulma.Degrees = -90;
+		} 
+		else if (kulma.Degrees > 0 && kulma.Degrees < 90)
+		{
+			kulma.Degrees = 90;
+		}
+		hahmo.Angle = kulma;
 		hahmo.Walk (nopeus);
 		AnimoiSammakko (sammakko.Animation = new Animation (sammakkoAnimKuvat), true);
 	}
 
+
 	public void LoikiYlos(PlatformCharacter hahmo, double nopeus)
 	{
-		MessageDisplay.Add (hahmo.Angle.Degrees.ToString());
+		kulma.Degrees = 0;
+		hahmo.Angle = kulma;
 		hahmo.ForceJump (nopeus);
 		if (flipped) {
 			hahmo.FlipImage ();
@@ -127,9 +156,12 @@ public class SammakkoKadulla : PhysicsGame
 		AnimoiSammakko (sammakko.Animation = new Animation (sammakkoAnimKuvat), true);
 	}
 
+
 	public void LoikiAlas(PlatformCharacter hahmo, double nopeus)
 	{
 		hahmo.ForceJump (nopeus);
+		kulma.Degrees = 1;
+		hahmo.Angle = kulma;
 		if (!flipped) {
 			hahmo.FlipImage ();
 			flipped = true;
@@ -137,6 +169,7 @@ public class SammakkoKadulla : PhysicsGame
 	
 		AnimoiSammakko (sammakko.Animation = new Animation (sammakkoAnimKuvat), true);
 	}
+
 
 	public void AnimoiSammakko(Animation animaatio, bool tilanne)
 	{
@@ -148,9 +181,29 @@ public class SammakkoKadulla : PhysicsGame
 		}
 	}
 
-	public void KaannaSammakko(Direction suunta)
+
+	void LuoPensaat(double x, double y,double leveys, double korkeus)
 	{
-		
+		PhysicsObject pensasVaalea = new PhysicsObject(leveys, korkeus -2);
+		PhysicsObject pensasTumma = new PhysicsObject(leveys, korkeus);
+		pensasVaalea.Image = pensasVaaleaKuva;
+		pensasTumma.Image = pensasTummaKuva;
+		pensasVaalea.Mass = 1200;
+		pensasTumma.Mass = 1200;
+		pensasTumma.Tag = "pTumma";
+		pensasVaalea.Tag = "pVaalea";
+		for (int i = 0; i <= 20; i++) 
+		{
+			pensasTumma.X = x;
+			pensasVaalea.X = x + 50;
+			pensasTumma.Y = y;
+			pensasVaalea.Y = y;
+			Add (pensasTumma, -1);
+			Add (pensasVaalea, -1);
+			x = i + 1;
+		}
 	}
+
+
 }
 
